e22567fa04ed5ba37587e1b19922e02e91cfdd83
JohnStotko-unplugged <johnstotko@gmail.com>
2025-11-28 13:20:10 -0600

    add code + makefile

---
+++ src/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+
+const char *VERSION = "1.0.0";
+
+void printVersion()
+{
+    printf("Program Version: %s\n", VERSION);
+}
+
+void printHelp()
+{
+    printf("Usage: program [options]\n");
+    printf("Options:\n");
+    printf("  -h, --help    Show this help message\n");
+    printf("  -v, --version Show program version\n");
+    printf("  <filename>    Specify input file. If absent, program \n");
+    printf("                will read from standard input\n");
+}
+
+
+
+int main(int argc, char *argv[])
+{
+    FILE* fp;
+    fp = stdin;
+
+    if(argc == 2)
+    {
+        char* arg = argv[1];
+        
+        if(strcmp(arg, "-h") == 0 || strcmp(arg, "--help") == 0)
+        {
+            printHelp();
+            return 0;
+        }
+
+        if(strcmp(arg, "-v") == 0 || strcmp(arg, "--version") == 0)
+        {
+            printVersion();
+            return 0;
+        }
+
+        fp = fopen(arg, "r");
+        if(fp == NULL)
+        {
+            perror("Error opening file");
+            return 1;
+        }
+    }
+    
+    if (fp != stdin)
+		(void)fclose(fp);
+
+    return 0;
+}
+
+
+
+
